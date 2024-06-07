e163b99f7f95468d8385013d0d9bc2aa683233c6
Sackitey68 <sackitey68@gmail.com>
2024-06-07 01:13:53 +0000

    Add intersection observer

--- main.js
+++ main.js
@@ -1,7 +1,33 @@
+"use strict";
+
 const btnNav = document.querySelector(".btn-mobile-nav");
 
 const header = document.querySelector("header");
 
 btnNav.addEventListener("click", () => {
   header.classList.toggle("nav-open");
+});
+
+// window.addEventListener("beforeunload", function (e) {
+//   e.preventDefault();
+//   e.returnValue = "";
+// });
+
+const allSections = document.querySelectorAll("section");
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove("section--hidden");
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.1,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
 });
